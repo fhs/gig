3d15cf6d07b4572cd7f974f35b26d580bdb162e7
Fazlul Shahriar <fshahriar@gmail.com>
2020-11-13 15:56:49 -0500

    Add an one line program documentation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the
 // license that can be found in the LICENSE file.
 
+// Program gig is a clone of git command.
 package main
 
 import "github.com/fhs/gig/commands"
