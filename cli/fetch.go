f4de4b61bf0168b6c8bf497087a59e3d4f67cff8
Fazlul Shahriar <fshahriar@gmail.com>
2020-11-28 17:31:53 -0500

    fetch: add new sub-command

---
+++ cli/fetch.go
@@ -0,0 +1,37 @@
+// Copyright 2020 Fazlul Shahriar. All rights reserved.
+// Use of this source code is governed by the
+// license that can be found in the LICENSE file.
+
+package cli
+
+import (
+	git "github.com/go-git/go-git/v5"
+	"github.com/spf13/cobra"
+)
+
+// fetchCmd represents the fetch command
+var fetchCmd = &cobra.Command{
+	Use:   "fetch [remote]",
+	Short: "Download objects and refs from another repository",
+	Long:  `If remote is not specified, the remote named origin is used.`,
+	Args:  cobra.MaximumNArgs(1),
+	RunE:  gitFetch,
+}
+
+func gitFetch(cmd *cobra.Command, args []string) error {
+	_, r, err := openRepo()
+	if err != nil {
+		return err
+	}
+	remote := "origin"
+	if len(args) >= 1 {
+		remote = args[0]
+	}
+	return r.Fetch(&git.FetchOptions{
+		RemoteName: remote,
+	})
+}
+
+func init() {
+	rootCmd.AddCommand(fetchCmd)
+}
