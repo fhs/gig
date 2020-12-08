46567378dd4bc3cd6fe7a27a153b3fb311e4e044
Fazlul Shahriar <fshahriar@gmail.com>
2020-12-07 21:28:58 -0500

    clone: make directory argument optional

--- cli/clone.go
+++ cli/clone.go
@@ -6,34 +6,63 @@ package cli
 
 import (
 	"fmt"
+	"path"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	cmd := &cobra.Command{
-		Use:   "clone url directory",
+		Use:   "clone url [directory]",
 		Short: "Clone a repository into a new directory",
-		Long:  ``,
-		RunE:  cloneCmd,
+		Long: `Clone a repository into a new directory. If the directory is not privided,
+it's derived from the URL.`,
+		Args: cobra.MaximumNArgs(2),
+		RunE: cloneCmd,
 	}
 	rootCmd.AddCommand(cmd)
 }
 
 func cloneCmd(_ *cobra.Command, args []string) error {
-	if len(args) < 2 {
-		return fmt.Errorf("usage: gig clone <url> <dir>")
+	if len(args) < 1 {
+		return fmt.Errorf("no URL provided")
 	}
-	auth, err := endpointAuth(args[0])
+	url := args[0]
+
+	dir := ""
+	if len(args) >= 2 {
+		dir = args[1]
+	} else {
+		ep, err := transport.NewEndpoint(url)
+		if err != nil {
+			return err
+		}
+		dir = cloneDir(ep.Path)
+	}
+
+	auth, err := endpointAuth(url)
 	if err != nil {
 		return err
 	}
-	// Clone the given repository to the given directory
-	_, err = git.PlainClone(args[1], false, &git.CloneOptions{
-		URL:      args[0],
+	fmt.Printf("Cloning into %q...\n", dir)
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL:      url,
 		Auth:     auth,
 		Progress: progressWriter,
 	})
 	return err
+}
+
+// cloneDir return the directory where newly cloned repository will be stored
+// based on the full path in git URL. For example, it converts
+// fhs/gig to gig, and fhs/gig.git to gig.
+func cloneDir(dir string) string {
+	d := path.Base(dir)
+	if suf := ".git"; strings.HasSuffix(d, suf) && len(d) > len(suf) {
+		d = d[:len(d)-len(suf)]
+	}
+	return d
 }
